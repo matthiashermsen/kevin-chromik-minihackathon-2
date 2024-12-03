bbe0f951bc0d0e1f1c9126018d451d5e5382b1ee
matthiashermsen <matthiashermsen@gmail.com>
2024-12-03 23:10:43 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+	const appVersion = __APP_VERSION__;
+
+	return <div>App version: {appVersion}</div>;
+}
+
+export { App };
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	const queryClient = new QueryClient();
+
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</React.StrictMode>,
+	);
+}
